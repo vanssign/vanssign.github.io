214e94c349db62a3f324faa2b288c607bfa71a2f
vanssign <thindvansh1999@gmail.com>
2021-03-31 00:35:04 +0530

    framer motion transitions

--- pages/home.js
+++ pages/home.js
@@ -1,69 +1,91 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import { motion } from "framer-motion"
 
 export default function Home() {
     return (
-      <div className={styles.container}>
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-  
-        <main className={styles.main}>
-          <h1>
-            Under Construction <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-          <input></input>
-          <br/>
-          <br/>
-          <button>Sign Up</button>
-          <p className={styles.description}>
-            Get started by editing{' '}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-  
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h3>HOME &rarr;</h3>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-  
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h3>Learn &rarr;</h3>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-  
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h3>Examples &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-  
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h3>Deploy &rarr;</h3>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <motion.figure style={{ float: 'left' }}
+                    layoutId="nextjsLogo"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.2 }}>
+                    <Image
+                        src="/nextjs.svg"
+                        alt="nextjs Logo"
+                        width={100}
+                        height={50}
+                    />
+                </motion.figure>
+                <h1>
+                    Under Construction <a href="https://nextjs.org">Next.js!</a>
+                </h1>
+                <input></input>
+                <br />
+                <br />
+                <button>Sign Up</button>
+                <p className={styles.description}>
+                    Get started by editing{' '}
+                    <code className={styles.code}>pages/index.js</code>
+                </p>
+
+                <div className={styles.grid}>
+                    <a href="https://nextjs.org/docs" className={styles.card}>
+                        <h3>HOME &rarr;</h3>
+                        <p>Find in-depth information about Next.js features and API.</p>
+                    </a>
+
+                    <a href="https://nextjs.org/learn" className={styles.card}>
+                        <h3>Learn &rarr;</h3>
+                        <p>Learn about Next.js in an interactive course with quizzes!</p>
+                    </a>
+
+                    <a
+                        href="https://github.com/vercel/next.js/tree/master/examples"
+                        className={styles.card}
+                    >
+                        <h3>Examples &rarr;</h3>
+                        <p>Discover and deploy boilerplate example Next.js projects.</p>
+                    </a>
+
+                    <a
+                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        className={styles.card}
+                    >
+                        <h3>Deploy &rarr;</h3>
+                        <p>
+                            Instantly deploy your Next.js site to a public URL with Vercel.
               </p>
-            </a>
-          </div>
-        </main>
-  
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
-      </div>
+                    </a>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by
+                    <motion.figure layoutId="vercelLogo" initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.2 }}>
+                        <Image
+                            src="/vercel.svg"
+                            alt="vercel Logo"
+                            width={75}
+                            height={75}
+                        />
+                    </motion.figure>
+                </a>
+            </footer>
+        </div>
     )
-  }
-  
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image';
 import styles from '../styles/Base.module.css'
+import { motion } from 'framer-motion';
 
 export default function Base() {
   return (
@@ -9,29 +10,43 @@ export default function Base() {
       <Head>
         <title>vanssign | EXPLORE</title>
       </Head>
-      <div>
+      <motion.figure layoutId="nextjsLogo" initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2 }}>
         <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
+          src="/nextjs.svg"
+          alt="nextjs Logo"
           width={250}
           height={200}
         />
-      </div>
+      </motion.figure>
       <Link href="/home">
-        <a className={styles.CTA}>EXPLORE</a></Link>
+        <motion.a className={styles.CTA}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2 }}
+        >EXPLORE</motion.a></Link>
 
       <div className={styles.tag}>
-        <p>DESIGN | DEVELOP | EXPERIENCE</p>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2 }}
+        >DESIGN | DEVELOP | EXPERIENCE</motion.p>
       </div>
-      <div>Developed in&nbsp;</div>
-      <div>
+      <motion.div initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2 }}>Hosted at&nbsp;</motion.div>
+      <motion.figure layoutId="vercelLogo" initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2 }}>
         <Image
-          src="/nextjs.svg"
-          alt="Nextjs Logo"
+          src="/vercel.svg"
+          alt="vercel Logo"
           width={100}
           height={50}
         />
-      </div>
+      </motion.figure>
     </div>
   )
 }
